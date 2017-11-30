6405381b228a8d2e3171c627c9ca2f166bbc8959
Son Do <hacphin.vs0411@gmail.com>
2017-11-30 17:18:10 +0700

    private-chat

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get('/demo',(req, res) => {
 const users = [];
 
 io.on('connection', socket => {
-    
+
     socket.on('client-send-username', username => {
         username_check = users.map(function(e) { return e.username; }).indexOf(username);
         if(username_check === 0){
@@ -32,84 +32,130 @@ io.on('connection', socket => {
             socket.username = username;
             socket.emit('server-send-dk-thanhcong', username);
             io.emit('server-send-list-user', users);
-            io.emit('server-send-list-room', arrayRoom());
         }
         
     });
 
-    socket.on('client-logout', () =>{
-        users.splice(
-            users.indexOf(socket.username),1
-        );
-        socket.broadcast.emit('server-send-list-user', users);
-        socket.broadcast.emit('server-send-ngung-go');
-    });
-
-    socket.on('change-room-user', data => {
-        console.log(data.idrooms);
-        console.log(data.inroom);
-
-        if(socket.phong) socket.leave(socket.phong);
-        socket.join(data.idrooms);
-        socket.phong = data.idrooms;
-
-        console.log('========');
-        console.log(socket.adapter.rooms);
-    });
-
+    //private chat user
     socket.on('client-send-message-private', data => {
-        
-        
-        console.log('========');
-        console.log(socket.adapter.rooms);
-        const dataMess = {username : socket.username, message : data.message, socketPhong : socket.phong};
-        io.sockets.in(socket.phong).emit('server-send-message-private', dataMess);
+        const dataMess = {username : socket.username, message : data.message, relayto: socket.id};
+        socket.broadcast.to(data.idroom).emit('server-send-message-private', dataMess);
+        socket.emit('rel-user', dataMess);
+    });
+    //check user typing
+    socket.on('user-typing', () => {
+        socket.broadcast.emit('user-typing', socket.username);        
+    });
+    socket.on('user-stop-typing', () => {
+        socket.broadcast.emit('user-stop-typing');        
     });
 
-    socket.on('toi-dang-go', () => {
-        socket.broadcast.emit('server-send-co-nguoi-dang-go', socket.username);        
-    });
-    socket.on('toi-ngung-go', () => {
-        socket.broadcast.emit('server-send-ngung-go');        
-    });
 
-    //socket.adapter.rooms show danh sach room dang co
-    socket.on('tao-room', room => {
-        const nameRoom = `room_${room}`
-        socket.join(nameRoom);
-        socket.phong = nameRoom;
-        io.emit('server-send-list-room', arrayRoom());
-        socket.emit('server-send-room-socket', room);
-        
-        console.log("===rom===");
-        console.log(socket.adapter.rooms);
-        console.log("======");
-        arrayRoomuser();
-        
-    });
-    socket.on('user-chat-room', data => {
-        io.sockets.in(socket.phong).emit('server-send-mess-chatroom', data);
-    });
-
-    
-
-    function arrayRoom(){
-        const arrRoom = [];
-        for(i in socket.adapter.rooms){
-            let checkRoom = i.indexOf('room_');
-            if(i.indexOf('room_') === 0 )  arrRoom.push(i);
-        };
-        return  arrRoom;
-    };
-    function arrayRoomuser(){
-        const arrRoomuser = [];
-        for(i in socket.adapter.rooms){
-            console.log(i);
-        };
-       // return  arrRoomuser;
-    };
-    
 });
+// io.on('connection', socket => {
+    
+//     socket.on('client-send-username', username => {
+//         username_check = users.map(function(e) { return e.username; }).indexOf(username);
+//         if(username_check === 0){
+//             //that bai
+//             socket.emit('server-send-dk-thatbai');
+//         }else{
+//             if(username === '') return socket.emit('server-send-username-notnull');
+//             const user = ({username: username, socketId: socket.id});
+//             users.push(user);
+//             socket.username = username;
+//             socket.emit('server-send-dk-thanhcong', username);
+//             io.emit('server-send-list-user', users);
+//             io.emit('server-send-list-room', arrayRoom());
+//         }
+        
+//     });
+
+//     socket.on('client-logout', () =>{
+//         users.splice(
+//             users.indexOf(socket.username),1
+//         );
+//         socket.broadcast.emit('server-send-list-user', users);
+//         socket.broadcast.emit('server-send-ngung-go');
+//     });
+
+//     socket.on('change-room-user', data => {
+//         console.log(data.idrooms);
+//         console.log(data.inroom);
+//         console.log(socket.phong);
+
+//         if(socket.phong) socket.leave(socket.phong);
+//         const room = data.idrooms;
+//         socket.join(room);
+//         socket.phong = `${room}vs${data.inroom}`;
+
+//         console.log('====socket.phong====');
+//         console.log(socket.phong);
+//         console.log('====socket.adapter.rooms====');
+//         console.log(socket.adapter.rooms);
+        
+        
+        
+        
+//     });
+
+//     socket.on('client-send-message-private', data => {
+//         // const room = data.idroom;
+//         // socket.join(room);
+//         // socket.phong = room;
+        
+//         console.log('========');
+//         console.log(socket.adapter.rooms);
+//         const dataMess = {username : socket.username, message : data.message, socketPhong : socket.phong};
+//         //socket.broadcast.to
+//         socket.broadcast.to(data.idroom).emit('server-send-message-private', dataMess);
+//         socket.emit('rel-user', dataMess);
+//     });
+
+//     socket.on('toi-dang-go', () => {
+//         socket.broadcast.emit('server-send-co-nguoi-dang-go', socket.username);        
+//     });
+//     socket.on('toi-ngung-go', () => {
+//         socket.broadcast.emit('server-send-ngung-go');        
+//     });
+
+//     //socket.adapter.rooms show danh sach room dang co
+//     socket.on('tao-room', room => {
+//         const nameRoom = `room_${room}`
+//         socket.join(nameRoom);
+//         socket.phong = nameRoom;
+//         io.emit('server-send-list-room', arrayRoom());
+//         socket.emit('server-send-room-socket', room);
+        
+//         console.log("===rom===");
+//         console.log(socket.adapter.rooms);
+//         console.log("======");
+//         arrayRoomuser();
+        
+//     });
+//     socket.on('user-chat-room', data => {
+//         io.sockets.in(socket.phong).emit('server-send-mess-chatroom', data);
+//     });
+
+    
+
+//     function arrayRoom(){
+//         const arrRoom = [];
+//         for(i in socket.adapter.rooms){
+//             let checkRoom = i.indexOf('room_');
+//             if(i.indexOf('room_') === 0 )  arrRoom.push(i);
+//         };
+//         return  arrRoom;
+//     };
+//     function arrayRoomuser(){
+//         const arrRoomuser = [];
+//         for(i in socket.adapter.rooms){
+//             console.log(i);
+//         };
+//        // return  arrRoomuser;
+//     };
+    
+// });
 
 
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,23 @@
 const socket = io('http://localhost:3000');
 
-
 $(document).ready(function(){
-    
+    //Array save message of all user
+    const ArrayMessage = [];
+
     $('.signintrue').hide();
+    $('.loadingtext').hide();
+
+    $('#logout').click(function(){
+        socket.emit('client-logout')
+        $('.formsignin').show(500);
+        $('.signintrue').hide(500);
+    });
     $('.formsignin').submit( (e) => {
         e.preventDefault();
         const username = $('input[name="username"]').val();
         socket.emit('client-send-username', username);
         $('input[name="username"]').val('');
     });
-
     socket.on('server-send-dk-thatbai', () => {
         alert('Ten dang ky da ton tai !');
     });
@@ -34,89 +41,8 @@ $(document).ready(function(){
             if(socket.id !== socketId){
                 ulAppend.append(`<li title="${socketId}"><img src="images/ava.jpg" alt=""><span id="${socketId}_li">${username}</span></li>`);
             }
-            
-        });
-        
-    });
-
-    $('#logout').click(function(){
-        socket.emit('client-logout')
-        $('.formsignin').show(500);
-        $('.signintrue').hide(500);
-    });
-
-    //chat form
-    $('.formchat').submit( e => {
-        e.preventDefault();
-        const message = $('input[name="message"]').val();
-        const idroom = $('#idRoom').val();
-        const inroom = $('#inroom').val();
-        socket.emit('client-send-message-private', {idroom, message});
-        $('input[name="message"]').val('');
-    });
-    socket.on('server-send-message-private', dataMess => {
-        const { username , message, socketPhong } = dataMess;
-        console.log('socket.id : ' + socket.id );
-        console.log('socketPhong : ' + socketPhong);
-        //console.log(id);
-        $('#inroom').val(socketPhong);
-        const skp = socketPhong.replace("priChat_","");
-        console.log(skp);
-        if(socket.id === skp){
-            $('.showmessage').append(`<p class='ad'>${message} : ${username}</p>`);
-        }else{
-            $('.showmessage').append(`<p class="guest">${username} : ${message}</p>`);
-        }
-        // //private auto chat
-        // if(id !== socket.id){
-        //     $('.prv_username').html(username);
-        //     $('#idRoom').val(id);
-        // }
-        
-    });
-
-    $('#textmess').focusin( () => {
-        socket.emit('toi-dang-go');
-    });
-    $('.loadingtext').hide();
-    socket.on('server-send-co-nguoi-dang-go', username => {
-        $('.loadingtext').show();
-        $('.loadingtext span').html(`${username}`)
-    });
-    $('#txt_mess').focusin( () => {
-        socket.emit('toi-dang-go');
-    });
-    $('#txt_mess').focusout( () => {
-        socket.emit('toi-ngung-go');
-    });
-    socket.on('server-send-ngung-go', () => {
-        $('.loadingtext').hide();
-    });
-    //tao room
-    $('#btntaoroom').click(function(){
-        socket.emit('tao-room', $('#txtRoom').val());
-        $('#txtRoom').val('');
-    });
-    socket.on('server-send-list-room', arrRoom => {
-        const roomhave = $('.roomhave').html('');
-        $('.allroom').html('');
-        $('.allroom').append(arrRoom.length);
-        arrRoom.map( i => {
-            roomhave.append(`<li class="room">${i.slice(5)}</li>`)
         });
     });
-    socket.on('server-send-room-socket', room =>{
-        $('#stayroom').html('');
-        $('#stayroom').text(`${room}`)
-    });
-    $('#idchatroom').click(() => {
-        socket.emit('user-chat-room', $('#txtchatroom').val());
-    });
-    socket.on('server-send-mess-chatroom', data => {
-        $('.messchatroom').append(`<p>${data}</p>`)
-    });
-
-    //chat private user
 
     $( document ).on( "click", ".useronline span", function() {
         const idroom = $(this).attr('id');
@@ -125,13 +51,179 @@ $(document).ready(function(){
         const idRoom = $('#idRoom').val('');
         divUser.append(name_us);
         idRoom.val(idroom.replace("_li",""));
-        const inroom = $('#inroom').val();
-        const idrooms = $('#idRoom').val();
-        const data = {idrooms,inroom}
-        socket.emit('change-room-user', data);
+     
     });
-    
+    //chat form
+    $('.formchat').submit( e => {
+        e.preventDefault();
+        const message = $('input[name="message"]').val();
+        const idroom = $('#idRoom').val();
+        socket.emit('client-send-message-private', {idroom, message});
+        $('input[name="message"]').val('');
+    });
+
+    socket.on('server-send-message-private', dataMess => {
+        const { username , message, relayto } = dataMess;
+        $('.showmessage').append(`<p class="guest">${username} : ${message}</p>`);
+        const idroom = $('#idRoom').val(relayto);
+        $('.prv_username').html(username);
+        //save username to ArrayMessage
+        const ctent_mess = $('.showmessage').html();
+        ArrayMessage.push({username: username, message: ctent_mess});
+        console.log(ArrayMessage);
+
+    });
+    socket.on('rel-user', dataMess => {
+        const { username , message } = dataMess;
+       $('.showmessage').append(`<p class='ad'>${message}</p>`);
+    });
+
+    //loadding đang trả lời
+    $('#txt_mess').focusin( () => {
+        socket.emit('user-typing');
+    });
+    $('#txt_mess').focusout( () => {
+        socket.emit('user-stop-typing');
+    });
+    socket.on('user-typing', username => {
+        $('.loadingtext').show();
+        $('.loadingtext span').html(`${username}`)
+    });
+    socket.on('user-stop-typing', () => {
+        $('.loadingtext').hide();
+    });
+
+
+
+
+
+
+
 });
+
+// $(document).ready(function(){
+    
+//     $('.signintrue').hide();
+//     $('.formsignin').submit( (e) => {
+//         e.preventDefault();
+//         const username = $('input[name="username"]').val();
+//         socket.emit('client-send-username', username);
+//         $('input[name="username"]').val('');
+//     });
+
+//     socket.on('server-send-dk-thatbai', () => {
+//         alert('Ten dang ky da ton tai !');
+//     });
+//     socket.on('server-send-username-notnull', () => {
+//         alert('Ten khong duoc trong !');
+//     });
+//     socket.on('server-send-dk-thanhcong', username => {
+//         $('.formsignin').hide(2000);
+//         $('.signintrue').show(1000);
+//         $('.hiuser').text(username);
+//     });
+
+//     socket.on('server-send-list-user', users => {
+//         const ulAppend = $('.useronline');
+//         $('.num_user_ol').html(' ');
+//         const num_user_ol = users.length - 1;
+//         $('.num_user_ol').append(num_user_ol);
+//         ulAppend.html('');
+//         users.forEach(user => {
+//             const {username, socketId} = user;
+//             if(socket.id !== socketId){
+//                 ulAppend.append(`<li title="${socketId}"><img src="images/ava.jpg" alt=""><span id="${socketId}_li">${username}</span></li>`);
+//             }
+            
+//         });
+        
+//     });
+
+//     $('#logout').click(function(){
+//         socket.emit('client-logout')
+//         $('.formsignin').show(500);
+//         $('.signintrue').hide(500);
+//     });
+
+//     //chat form
+//     $('.formchat').submit( e => {
+//         e.preventDefault();
+//         const message = $('input[name="message"]').val();
+//         const idroom = $('#idRoom').val();
+//         const inroom = $('#inroom').val();
+//         socket.emit('client-send-message-private', {idroom, message});
+//         $('input[name="message"]').val('');
+//     });
+//     socket.on('server-send-message-private', dataMess => {
+//         const { username , message, socketPhong } = dataMess;
+//         $('.showmessage').append(`<p class="guest">${username} : ${message}</p>`);
+        
+//     });
+//     socket.on('rel-user', dataMess => {
+//         const { username , message, socketPhong } = dataMess;
+
+//        $('.showmessage').append(`<p class='ad'>${message} : ${username}</p>`);
+        
+//     });
+
+//     $('#textmess').focusin( () => {
+//         socket.emit('toi-dang-go');
+//     });
+//     $('.loadingtext').hide();
+//     socket.on('server-send-co-nguoi-dang-go', username => {
+//         $('.loadingtext').show();
+//         $('.loadingtext span').html(`${username}`)
+//     });
+//     $('#txt_mess').focusin( () => {
+//         socket.emit('toi-dang-go');
+//     });
+//     $('#txt_mess').focusout( () => {
+//         socket.emit('toi-ngung-go');
+//     });
+//     socket.on('server-send-ngung-go', () => {
+//         $('.loadingtext').hide();
+//     });
+//     //tao room
+//     $('#btntaoroom').click(function(){
+//         socket.emit('tao-room', $('#txtRoom').val());
+//         $('#txtRoom').val('');
+//     });
+//     socket.on('server-send-list-room', arrRoom => {
+//         const roomhave = $('.roomhave').html('');
+//         $('.allroom').html('');
+//         $('.allroom').append(arrRoom.length);
+//         arrRoom.map( i => {
+//             roomhave.append(`<li class="room">${i.slice(5)}</li>`)
+//         });
+//     });
+//     socket.on('server-send-room-socket', room =>{
+//         $('#stayroom').html('');
+//         $('#stayroom').text(`${room}`)
+//     });
+//     $('#idchatroom').click(() => {
+//         socket.emit('user-chat-room', $('#txtchatroom').val());
+//     });
+//     socket.on('server-send-mess-chatroom', data => {
+//         $('.messchatroom').append(`<p>${data}</p>`)
+//     });
+
+//     //chat private user
+    
+//     $( document ).on( "click", ".useronline span", function() {
+//         const idroom = $(this).attr('id');
+//         const name_us = $(this).html();
+//         const divUser = $('.prv_username').html('');
+//         const idRoom = $('#idRoom').val('');
+//         divUser.append(name_us);
+//         idRoom.val(idroom.replace("_li",""));
+//         $('#inroom').val(socket.id);
+//         const inroom = $('#inroom').val();
+//         const idrooms = $('#idRoom').val();
+//         const data = {idrooms,inroom}
+//         //socket.emit('change-room-user', data);
+//     });
+    
+// });
 
 function startTime() {
     var today = new Date();
